14315a6dfcf36954ae55e2086e808bb34f3a8391
FreddyA12 <2balvarezfreddy@gmail.com>
2024-05-21 15:50:10 -0500

    Se implemento el cifrado

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { DocenteService } from 'src/docente/docente.service';
 import { AuthPayloadDTO } from 'src/auth/dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,16 +12,20 @@ export class AuthService {
   ) {}
 
   async validateUser({ nombre_usuario, contrasenia }: AuthPayloadDTO) {
-    const findUser =
-      await this.docenteService.findOneByUsername(nombre_usuario);
+    const findUser = await this.docenteService.findOneByUsername(nombre_usuario);
 
-    if (!findUser) throw new UnauthorizedException('Credenciales invalidas');
+    if (!findUser) {
+      throw new UnauthorizedException('Credenciales invalidas');
+    }
 
-    if (contrasenia === findUser.contrasenia) {
-      const { contrasenia, persona, cargo, ...user } = findUser;
-      return this.jwtService.sign(user);
+    // Utiliza bcrypt.compare para verificar la contraseña
+    const isMatch = await bcrypt.compare(contrasenia, findUser.contrasenia);
+    if (isMatch) {
+      const { contrasenia, persona, cargo, ...userDetails } = findUser;
+      return this.jwtService.sign(userDetails); // Firmar y retornar el JWT
     }
 
     throw new UnauthorizedException('Credenciales invalidas');
   }
+
 }
--- src/docente/docente.service.ts
+++ src/docente/docente.service.ts
@@ -2,21 +2,31 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Docente } from 'src/entities/docente.entity';
 import { Repository } from 'typeorm';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
+
 export class DocenteService {
   constructor(
     @InjectRepository(Docente) private docentesRepository: Repository<Docente>,
   ) {}
   
-  save(docente: any): Promise<Docente> {
-    if (docente != null) {
-      if (docente.persona.identificacion.length == 10) {
-        return this.docentesRepository.save(docente);
-      }
+  
+  async save(docente: any): Promise<Docente> {
+    if (!docente) {
+      throw new BadRequestException('Datos faltantes');
+    }
+
+    if (docente.persona && docente.persona.identificacion.length !== 10) {
       throw new BadRequestException('El numero de identificacion no es valido');
     }
-    throw new BadRequestException('Datos faltantes');
+
+    if (docente.contrasenia) {
+      // Hashear la contraseña antes de guardarla
+      const salt = await bcrypt.genSalt();
+      docente.contrasenia = await bcrypt.hash(docente.contrasenia, salt);
+    }
+
+    return this.docentesRepository.save(docente);
   }
 
   async findOneByUsername(
