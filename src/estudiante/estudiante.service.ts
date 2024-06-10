0b05980a7b1050c10839162faa9092dcec30123c
nanobotrs <raansopro324@gmail.com>
2024-06-10 10:07:54 -0500

    Correccion del metodo get para obtener estudiante por id

--- src/estudiante/estudiante.service.ts
+++ src/estudiante/estudiante.service.ts
@@ -19,6 +19,8 @@ export class EstudianteService {
     private estudianteRepository: Repository<Estudiante>,
     @InjectRepository(Docente)
     private docenteRepository: Repository<Docente>,
+    @InjectRepository(Persona)
+    private personaRepository: Repository<Persona>,
     private titulacionService: TitulacionService,
   ) {}
 
@@ -32,13 +34,11 @@ export class EstudianteService {
       throw new NotFoundException('Docente no encontrado');
     }
 
-    const estudianteExists = await this.estudianteRepository.findOneBy({
-      persona: {
-        identificacion: createEstudianteDto.persona.identificacion,
-      },
+    const personaExists = await this.personaRepository.findOneBy({
+      identificacion: createEstudianteDto.persona.identificacion,
     });
 
-    if (estudianteExists) {
+    if (personaExists) {
       throw new ConflictException('El estudiante ya existe');
     }
 
@@ -52,7 +52,7 @@ export class EstudianteService {
 
     const estudiante = new Estudiante();
     estudiante.carrera = createEstudianteDto.carrera;
-    estudiante.estado = 'En progreso';
+    estudiante.estado = 'En proceso';
     // Crear persona
     const persona = new Persona(); // Asumiendo que tienes un constructor que crea el objeto Persona
     persona.identificacion = createEstudianteDto.persona.identificacion;
@@ -119,15 +119,14 @@ export class EstudianteService {
 
     throw new NotFoundException('El docente no tiene estudiantes asignados');
   }
-  async getEstudianteById(id_estudiante: number): Promise<Estudiante | undefined> {
-
+  async getEstudianteById(
+    id_estudiante: number,
+  ): Promise<Estudiante | undefined> {
     const estudiante = await this.estudianteRepository.findOne({
       where: { id: id_estudiante },
       relations: ['persona', 'titulacion'],
     });
-    
+
     return estudiante;
-
   }
-
 }
